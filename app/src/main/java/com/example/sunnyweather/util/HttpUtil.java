73b7172f436722881096c5fd213b293bd070b529
wanglingfeng <wanglingfeng@xiaomi.com>
2021-10-29 19:16:30 +0800

    10-29

--- app/src/main/java/com/example/sunnyweather/util/HttpUtil.java
+++ app/src/main/java/com/example/sunnyweather/util/HttpUtil.java
@@ -1,5 +1,7 @@
 package com.example.sunnyweather.util;
 
+import android.util.Log;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -7,6 +9,7 @@ public class HttpUtil {
     public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
+        Log.i("TAG", "sendOkHttpRequest: "+request.toString());
         client.newCall(request).enqueue(callback);
     }
 }
--- app/src/main/java/com/example/sunnyweather/util/Utility.java
+++ app/src/main/java/com/example/sunnyweather/util/Utility.java
@@ -65,7 +65,10 @@ public class Utility {
                 for (int i = 0;i < allCounties.length();i++){
                     JSONObject countyObject = allCounties.getJSONObject(i);
                     County county = new County();
-
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
                 }
                 return true;
             }catch (JSONException e){
